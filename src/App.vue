d1f1e3e0d9007cdd291993d92ba030ace6f4b57a
Himanshu <himanshu.garg@indiap2p.com>
2022-02-10 12:34:05 +0530

    empty

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <h3>Line chart with a single dataset.....</h3>
+      <LineChart/>
+    </div>
+  </div>
+</template>
+
+<script>
+import LineChart from './components/LineChart.vue'
+
+export default {
+  components:{
+    LineChart
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #0000;
+}
+</style>
---
+++ src/components/LineChart.vue
@@ -0,0 +1,58 @@
+<script>
+  import { Line } from 'vue-chartjs'
+
+  export default {
+    extends: Line,
+    data () {
+      return {
+        datacollection: {
+          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+          datasets: [
+            {
+              label: 'Data One',
+              pointBackgroundColor: '#F25207',
+              borderWidth: 5,
+              fill:false,
+              pointBorderColor: '#F25207',
+              data: [60, 40, 20, 50, 90, 10, 20, 40, 50, 70, 90, 100]
+            },
+            {
+              label: 'Data Two',
+              pointBackgroundColor: 'white',
+              borderWidth: 1,
+              opacity:0.5,
+              backgroundColor:"#F2D707",
+              pointBorderColor: '#249EBF',
+              data: [20, 60, 10, 80, 30, 50, 60, 20, 30, 40, 70, 20]
+            }
+          ]
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true
+              },
+              gridLines: {
+                display: true
+              }
+            }],
+            xAxes: [ {
+              gridLines: {
+                display: false
+              }
+            }]
+          },
+          legend: {
+            display: true
+          },
+          responsive: true,
+          maintainAspectRatio: false
+        }
+      }
+    },
+    mounted () {
+      this.renderChart(this.datacollection, this.options)
+    }
+  }
+</script>
